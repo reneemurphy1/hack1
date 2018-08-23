62190f99610f1f6349d6ac1e8b6e2548d4cf668a
Renee Murphy <renee.murphy1@yahoo.com>
2018-08-23 12:40:40 -0500

    hello world

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Renee Murphy and Dylan Spotanski
+ * Date: 2018/08/21
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Renee Murphy and Dylan Spotanski\n");
+
+  return 0;
+}
